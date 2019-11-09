9b45b810399d1bdf2257d46ae5f9937df92076ab
baishucong <1015289039@qq.com>
2019-11-09 14:23:33 +0800

    twuhdioadhiasjdkkasmd;lsa

--- sk-project-food/src/main/java/com/sk/controller/IndexmenuController.java
+++ sk-project-food/src/main/java/com/sk/controller/IndexmenuController.java
@@ -2,6 +2,7 @@ package com.sk.controller;
 
 import com.sk.dao.IndexmenuMapper;
 import com.sk.entity.Indexmenu;
+import com.sun.org.apache.xpath.internal.objects.XString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ public class IndexmenuController {
 
     @Autowired
     private IndexmenuMapper indexmenuMapper;
+    private XString username;
 
     @ResponseBody
     @RequestMapping("/getIndexmenu")
